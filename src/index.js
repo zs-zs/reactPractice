598fe0de2c7ab82c25d3585856978f1aa55c7a71
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-11-21 21:20:02 +0100

    use browserHistory in router config

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './index.css'
 import React from 'react'
 import {render} from 'react-dom'
 
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from './App';
 import TableSoccerScores from './TableSoccerScores';
 import About from './About';
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={TableSoccerScores} />
       <Route path="scores" component={TableSoccerScores} />
