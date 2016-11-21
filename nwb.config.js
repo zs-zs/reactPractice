95b367d1d6dd35662f01dee4b9fa4948e88a8fc2
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-11-21 21:20:02 +0100

    improve karma config

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   type: 'react-app'
+	karma: {
+		browsers: ['Chrome', 'Firefox', 'IE'],
+		//browsers: ['IE8'],
+		extra: {
+			//singleRun: false,
+			customLaunchers: {
+				IE9: {
+					base: 'IE',
+					'x-ua-compatible': 'IE=EmulateIE9'
+				},
+				IE8: {
+					base: 'IE',
+					'x-ua-compatible': 'IE=EmulateIE8'
+				}
+			}
+		},
+		plugins: [
+			require('karma-firefox-launcher'),
+			require('karma-ie-launcher')
+		]
+	}
 }
