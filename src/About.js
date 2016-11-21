b5845cdf96a3abbe92d0042c2a80c3d334cf85e3
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-11-21 21:20:02 +0100

    import an image file on the about page

--- src/About.js
+++ src/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import logo from './techtalk_logo.png';
 
-const About = () => (<div>TechTalk 2016</div>);
+const About = () => (<div>TechTalk 2016<img src={logo} width="200" /></div>);
 
 export default About;
