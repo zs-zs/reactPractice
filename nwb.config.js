213213875fbb894911141e246ec36262d2458794
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-11-22 18:15:59 +0100

    test can be executed on jsdom

--- nwb.config.js
+++ nwb.config.js
@@ -6,24 +6,24 @@ module.exports = {
 		}
 	},
 	karma: {
-		browsers: ['Chrome', 'Firefox', 'IE'],
+		//browsers: ['Chrome', 'Firefox', 'IE'],
 		//browsers: ['IE8'],
-		extra: {
-			//singleRun: false,
-			customLaunchers: {
-				IE9: {
-					base: 'IE',
-					'x-ua-compatible': 'IE=EmulateIE9'
-				},
-				IE8: {
-					base: 'IE',
-					'x-ua-compatible': 'IE=EmulateIE8'
-				}
-			}
-		},
-		plugins: [
-			require('karma-firefox-launcher'),
-			require('karma-ie-launcher')
-		]
+		// extra: {
+		// 	//singleRun: false,
+		// 	customLaunchers: {
+		// 		IE9: {
+		// 			base: 'IE',
+		// 			'x-ua-compatible': 'IE=EmulateIE9'
+		// 		},
+		// 		IE8: {
+		// 			base: 'IE',
+		// 			'x-ua-compatible': 'IE=EmulateIE8'
+		// 		}
+		// 	}
+		// },
+		// plugins: [
+		// 	require('karma-firefox-launcher'),
+		// 	require('karma-ie-launcher')
+		// ]
 	}
 }
